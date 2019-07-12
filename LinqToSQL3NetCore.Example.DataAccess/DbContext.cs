9a2afe0ce56ce00964b7fad3e7303345ee59cffc
copiltembel <37596026+copiltembel@users.noreply.github.com>
2019-07-12 12:56:01 +0200

    Leftovers

--- LinqToSQL3NetCore.Example.DataAccess/DbContext.cs
+++ LinqToSQL3NetCore.Example.DataAccess/DbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Db.DataAccess.DataSet;
 using System.Collections.Generic;
+using System.Data.Linq;
 
 namespace LinqToSQL3NetCore.Example.DataAccess
 {
@@ -10,24 +11,29 @@ namespace LinqToSQL3NetCore.Example.DataAccess
         {
         }
 
-        public virtual void DeleteOnSubmit<TDbEntity>(IReadOnlyList<TDbEntity> dbEntities) where TDbEntity : class
+        public virtual void DeleteOnSubmit<TDbEntity>(IReadOnlyList<TDbEntity> dbEntities) where TDbEntity : DbEntityBase
         {
             this.GetTable<TDbEntity>().DeleteAllOnSubmit(dbEntities);
         }
 
-        public virtual void InsertOnSubmit<TDbEntity>(IReadOnlyList<TDbEntity> dbEntities) where TDbEntity : class
+        public virtual void InsertOnSubmit<TDbEntity>(IReadOnlyList<TDbEntity> dbEntities) where TDbEntity : DbEntityBase
         {
             this.GetTable<TDbEntity>().InsertAllOnSubmit(dbEntities);
         }
 
-        public virtual void InsertOnSubmit<TDbEntity>(TDbEntity dbEntity) where TDbEntity : class
+        public virtual void InsertOnSubmit<TDbEntity>(TDbEntity dbEntity) where TDbEntity : DbEntityBase
         {
             this.GetTable<TDbEntity>().InsertOnSubmit(dbEntity);
         }
 
-        public virtual void DeleteOnSubmit<TDbEntity>(TDbEntity dbEntity) where TDbEntity : class
+        public virtual void DeleteOnSubmit<TDbEntity>(TDbEntity dbEntity) where TDbEntity : DbEntityBase
         {
             this.GetTable<TDbEntity>().DeleteOnSubmit(dbEntity);
+        }
+
+        public virtual void DeleteAllOnSubmit<TDbEntity>(IEnumerable<TDbEntity> dbEntities) where TDbEntity : DbEntityBase
+        {
+            this.GetTable<TDbEntity>().DeleteAllOnSubmit(dbEntities);
         }
     }
 }
--- LinqToSQL3NetCore.Example/Program.cs
+++ LinqToSQL3NetCore.Example/Program.cs
@@ -15,20 +15,13 @@ namespace LinqToSQL3NetCore.Example
 
             DbContext dbContext = new DbContext(connectionString);
 
-            //TestDeleteMultiple(dbContext);
-            TestLoadWhere(dbContext);
-            TestLoadMany(dbContext);
-            //var addresses = dbContext.Addresses.ToList();
+            RunTest(dbContext);
         }
 
-        private static void TestLoadMany(DbContext dbContext)
+        private static void RunTest(DbContext dbContext)
         {
-            var testTable1s = dbContext.TestTable1s.ToList();
-        }
-
-        private static void TestLoadWhere(DbContext dbContext)
-        {
-            var x = dbContext.Addresses.Where(a => a.Id == new DbId<Address, int>(1)).ToList();
+            var x = new TestInsertUpdateDelete(dbContext);
+            x.RunTest2();
         }
 
         static string GetConnectionString(string jsonFileName)
@@ -36,48 +29,6 @@ namespace LinqToSQL3NetCore.Example
             var connectionSettings = JsonConvert.DeserializeObject<ConnectionSettings>(File.ReadAllText(jsonFileName));
             var connectionStrings = connectionSettings.DbConnectionStrings.Db1;
             return connectionStrings;
-        }
-
-        static void InsertSomeEntities(DbContext dbContext)
-        {
-            for (int i = 0; i < 1000; i++)
-            {
-                var table1s = new TestTable1();
-                table1s.Dummy = "Testin";
-                table1s.Dummy2 = i;
-                dbContext.GetTable<TestTable1>().InsertOnSubmit(table1s);
-            }
-            dbContext.SubmitChanges();
-        }
-
-        static void TestInsertOrder(DbContext dbContext)
-        {
-            var testTable1 = new TestTable1();
-            testTable1.Dummy = "First";
-            testTable1.Dummy2 = 1;
-            var testTable3 = new TestTable3();
-            testTable3.TestTable1 = testTable1;
-            dbContext.GetTable<TestTable3>().InsertOnSubmit(testTable3);
-            dbContext.SubmitChanges();
-        }
-
-        static void TestDeleteMultiple(DbContext dbContext)
-        {
-            var testTable1 = new TestTable1();
-            testTable1.Dummy = "Del";
-            testTable1.Dummy2 = 69;
-            dbContext.GetTable<TestTable1>().InsertOnSubmit(testTable1);
-            var testTable31 = new TestTable3();
-            testTable31.TestTable1 = testTable1;
-            var testTable32 = new TestTable3();
-            testTable32.TestTable1 = testTable1;
-
-            dbContext.SubmitChanges();
-
-            dbContext.DeleteOnSubmit(testTable31);
-            dbContext.DeleteOnSubmit(testTable32);
-            dbContext.DeleteOnSubmit(testTable1);
-            dbContext.SubmitChanges();
         }
     }
 }
